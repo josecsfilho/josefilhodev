8f1e8f637d1f25d672d401befbf01d0fcb0b1a4e
José Filho <100032235+josecsfilho@users.noreply.github.com>
2023-08-29 00:07:56 -0300

    deploy

--- carreiradev/settings.py
+++ carreiradev/settings.py
@@ -119,10 +119,16 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'josefilhodev', 'static'),
+    os.path.join(BASE_DIR / 'josefilhodev/static'),
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'josefilhodev', 'static')
 
+# STATICFILES_DIRS = [
+#     # BASE_DIR / 'static',
+#     BASE_DIR / 'josefilhodev/static'
+# BASE_DIR, 'josefilhodev', 'static'
+# ]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 # MEDIA_URL = '/media/'
 # MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
