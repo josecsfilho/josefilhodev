3d72949a482421dd43eb64776c5bcd222aae81ee
José Filho <100032235+josecsfilho@users.noreply.github.com>
2023-08-28 22:12:19 -0300

    Migração do banco de dados para Postgres.

--- carreiradev/settings.py
+++ carreiradev/settings.py
@@ -74,13 +74,23 @@ WSGI_APPLICATION = 'carreiradev.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'verceldb',  # Nome do banco de dados
+        'USER': 'default',   # Nome do usuário
+        'PASSWORD': 'X1CmDZAdqt8c',  # Senha do usuário
+        'HOST': 'ep-muddy-moon-72810631.us-east-1.postgres.vercel-storage.com',  # Host do banco de dados
+        'PORT': '5432',  # Porta do banco de dados
+    }
 }
-
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'carreiradev.settings')
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
