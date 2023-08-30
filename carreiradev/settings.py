18f917136ad23b52b039b68bea86dfb7b75c2c11
José Filho <100032235+josecsfilho@users.noreply.github.com>
2023-08-29 23:01:12 -0300

    deploy

--- carreiradev/settings.py
+++ carreiradev/settings.py
@@ -130,13 +130,8 @@ USE_TZ = True
 # ]
 # STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
-
-if 'VERCEL' in os.environ:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
