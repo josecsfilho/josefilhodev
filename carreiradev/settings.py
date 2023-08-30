c39f7dbaa8732f1c290dcf03fa2ccb3243aafc86
José Filho <100032235+josecsfilho@users.noreply.github.com>
2023-08-29 22:40:12 -0300

    deploy

--- carreiradev/settings.py
+++ carreiradev/settings.py
@@ -3,9 +3,8 @@ import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+# BASE_DIR = Path(__file__).resolve().parent.parent
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
@@ -124,13 +123,20 @@ USE_TZ = True
 # ]
 #
 # STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+#
+# STATIC_URL = '/static/'
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'josefilhodev', 'static'),
+# ]
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'josefilhodev', 'static'),
+    os.path.join(BASE_DIR, 'static'),
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
+if 'VERCEL' in os.environ:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
