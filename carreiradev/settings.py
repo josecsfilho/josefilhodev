5d308c847d1257ba5e8f33ed9f31efcd80dda388
José Filho <100032235+josecsfilho@users.noreply.github.com>
2023-09-03 01:45:58 -0300

    Update settings.py
    Inserido dominio no HOST

--- carreiradev/settings.py
+++ carreiradev/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = 'django-insecure-799m6s#&zu%-_mf&)cyyn&675d^t9izs$_d00b$rbukcxpm2tq
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app', '.now.sh', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['.vercel.app', 'josefilho.tech', 'www.josefilho.tech', '.now.sh', 'localhost', '127.0.0.1']
 
 
 # Application definition
